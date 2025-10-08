13acd79a20d5df475d9ec6c300b61823b7399b1a
Chris <cristianortizventas@gmail.com>
2025-10-08 08:34:14 -0500

    workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '::',
     port: 8080,
+    open: true,
+  },
+  preview: {
+    port: 4173,
+    open: true,
   },
   plugins: [react()],
   resolve: {
